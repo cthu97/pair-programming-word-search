654ff8c269b5623505f0b8520e279ab47b26ea2a
Catherine Hu <catherinehu_9898@hotmail.com>
2021-06-30 23:13:31 +0000

    done

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,21 @@
-const wordSearch = (letters, word) => { 
+const wordSearch = (letters, word) => {
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+    let emptyStr = ""
+    for (let x = 0; x < letters[0].length; x++) {
+        for (let y = 0; y < letters.length; y++) {
+            emptyStr += letters[x][y];
+        }
+    }
+    if (emptyStr === word) {
+        return true;
+    }
+    return false;
 }
+
+
+
 
 module.exports = wordSearch
